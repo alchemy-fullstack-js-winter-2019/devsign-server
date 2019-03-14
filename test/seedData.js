82b19b816e84eaddb9f78da689f2e8aff1c9ebc6
Carmen Perezchica <crperezchica@gmail.com>
2019-03-14 14:14:00 -0700

    create post route and finished app

---
+++ test/seedData.js
@@ -0,0 +1,10 @@
+const tweet = require('../lib/models/Tweet');
+const chance = require('chance').Chance();
+
+function seedData() {
+  const tweetsToCreate = [...Array(100)].map (() => {
+    tw
+  })
+  handle: chance.name(),
+  text: chance.sentence()
+}
