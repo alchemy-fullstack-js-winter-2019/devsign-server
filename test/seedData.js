d2b38c86c3fd79558a04c36fbfa466822549f5e1
Carmen Perezchica <crperezchica@gmail.com>
2019-03-14 15:55:01 -0700

    seed data

--- test/seedData.js
+++ test/seedData.js
@@ -1,10 +1,13 @@
-const tweet = require('../lib/models/Tweet');
+const Tweet = require('../lib/models/Tweet');
 const chance = require('chance').Chance();
 
 function seedData() {
-  const tweetsToCreate = [...Array(100)].map (() => {
-    tw
-  })
-  handle: chance.name(),
-  text: chance.sentence()
+  const tweetsToCreate = [...Array(100)].map(() => ({
+    user: 'auth0|5c00b0cde9f0262e937842c9',
+    text: chance.sentence()
+  }));
+
+  return Tweet.create(tweetsToCreate);
 }
+
+module.exports = seedData;
