04088cc2e9b7efd8891b9648ec8de56bf0300705
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-15 11:21:33 -0700

    passing get route test

---
+++ tests/messages/routes.test.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+
+const app = require('../../lib/app');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const { seedDataMessage } = require('../seedData');
+
+jest.mock('../../lib/services/auth.js');
+jest.mock('../../lib/middleware/ensureAuth.js');
+
+describe('message routes', () => {
+  beforeEach(() => {
+    return seedDataMessage(100);
+  });
+  afterEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  it('can get a list of messages',() => {
+    return request(app)
+      .get('/messages')
+      .then(res => res.body)
+      .then(messages => {
+        expect(messages).toHaveLength(100);
+      });
+  });
+});
