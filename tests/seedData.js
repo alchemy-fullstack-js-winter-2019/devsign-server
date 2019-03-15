68857c83af6fd523ab6b125b32a48a50c9558771
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-15 14:31:50 -0700

    lots of things

--- tests/seedData.js
+++ tests/seedData.js
@@ -3,7 +3,7 @@ const chance = require('chance').Chance();
 
 function seedData(count = 100) {
     const tweetsToCreate = [...Array(count)].map(() => ({
-        user: 'auth0|5c00b0cde9f0262e937842c9',
+        user: 'google-oauth2|113221460541302189898',
         text: chance.sentence()
     }));
 
