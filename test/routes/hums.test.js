fc2dfb53c8c81865b6c9246e236a85db33da9af1
Paige Gorry <paigeegorry@gmail.com>
2019-03-15 10:07:08 -0700

    can get hums created by a specific user

--- test/routes/hums.test.js
+++ test/routes/hums.test.js
@@ -4,9 +4,15 @@ const seedData = require('../seedData.js');
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../lib/app');
+const Hum = require('../../lib/models/Hum');
 
 jest.mock('../../lib/services/auth.js');
 jest.mock('../../lib/middleware/ensureAuth.js');
+
+const createHums = (user) => {
+  return Hum.create({ user, hum: 'tweet chirp hum' })
+    .then(createdHum => ({ ...createdHum }));
+};
 
 describe('hums route', () => {
   beforeEach(() => {
@@ -45,7 +51,7 @@ describe('hums route', () => {
       });
   });
 
-  it.only('can delete a hum by id', () => {
+  it('can delete a hum by id', () => {
     return request(app)
       .post('/hums')
       .send({
@@ -61,6 +67,21 @@ describe('hums route', () => {
               _id: expect.any(String),
               hum: 'this is my hum'
             });
+          });
+      });
+  });
+
+  it('can get all hums created by a specific user', () => {
+    const humsToCreate = ['1234', '1234', '1224'];
+    return Promise.all(humsToCreate.map(createHums))
+      .then(() => {
+        return request(app)
+          .get('/hums/users/1234')
+          .then(res => {
+            expect(res.body).toEqual([
+              { '__v': 0, '_id': expect.any(String), 'hum': 'tweet chirp hum', 'user': '1234' }, 
+              { '__v': 0, '_id': expect.any(String), 'hum': 'tweet chirp hum', 'user': '1234' }
+            ]);
           });
       });
   });
