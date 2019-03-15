5eab344b350f22856172ebb1098541a736d6bba0
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-15 11:47:43 -0700

    added mock for ensureAuth

--- tests/tweets.test.js
+++ tests/tweets.test.js
@@ -8,6 +8,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 jest.mock('../lib/services/auth.js');
+jest.mock('../lib/middleware/ensureAuth.js');
 
 describe('tweets routes', () => {
   beforeEach(() => {
