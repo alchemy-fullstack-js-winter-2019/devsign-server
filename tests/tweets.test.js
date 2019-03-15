7472ba5e7ffe4ade5bd3043664fdf20cb3eb1a34
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-15 08:36:23 -0700

    added post and tests

--- tests/tweets.test.js
+++ tests/tweets.test.js
@@ -18,12 +18,48 @@ describe('tweets routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  it('posts a tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        text: 'Hello Tweet'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: 0,
+          user: '123456',
+          _id: expect.any(String),
+          text: 'Hello Tweet'
+        });
+      });
+  });
+
   it('can get a list of tweets', () => {
     return request(app)
       .get('/tweets')
       .then(res => res.body)
       .then(tweets => {
         expect(tweets).toHaveLength(100);
+      });
+  });
+
+  it('deletes a tweet by id', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        text: 'Goodbye tweet'
+      })
+      .then(res => {
+        return request(app)
+          .delete(`/tweets/${res.body._id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              __v: 0,
+              user: '123456',
+              _id: expect.any(String),
+              text: 'Goodbye tweet'
+            });
+          });
       });
   });
 });
