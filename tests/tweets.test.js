568c316b48ec07cdb59c67fa8446aee86c3db685
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-15 16:01:31 -0700

    something

--- tests/tweets.test.js
+++ tests/tweets.test.js
@@ -8,15 +8,15 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 jest.mock('../lib/services/auth.js');
+afterAll(() => {
+    mongoose.connection.close();
+});
 
-describe('tweets routes', async() => {
+describe('tweets routes', () => {
     beforeEach(() => {
         return seedData(100);
     });
 
-    afterAll(() => {
-        mongoose.connection.close();
-    });
 
     afterEach(() => {
         return mongoose.connection.dropDatabase();
