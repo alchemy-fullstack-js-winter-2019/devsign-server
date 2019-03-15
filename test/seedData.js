7bc7c73ea4a060ca71382f93f012a09bfaf96754
Carmen Perezchica <crperezchica@gmail.com>
2019-03-15 10:06:37 -0700

    seed data loading to robot3t

--- test/seedData.js
+++ test/seedData.js
@@ -3,7 +3,7 @@ const chance = require('chance').Chance();
 
 function seedData() {
   const tweetsToCreate = [...Array(100)].map(() => ({
-    user: 'auth0|NEEDTOFILL',
+    user: 'auth0|5c00b0cde9f0262e937842c9',
     text: chance.sentence()
   }));
 
