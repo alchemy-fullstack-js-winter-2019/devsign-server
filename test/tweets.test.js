fff3b07e2e521ea6d5e13df66df8abd057f4b6cb
Shaba9 <shaba92@gmail.com>
2019-03-16 15:23:38 -0700

    users route added; test passes

--- test/tweets.test.js
+++ test/tweets.test.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
-require('../lib/utils/connect')();
-
+const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const seedData = require('./seedData');
-
 const request = require('supertest');
 const app = require('../lib/app');
 
 jest.mock('../lib/services/auth.js');
 
 describe('tweets routes', () => {
+  beforeAll(() => connect());
+
   beforeEach(() => seedData(15, 50));
 
   afterEach(() => mongoose.connection.dropDatabase());
 
   afterAll(done => mongoose.connection.close(done));
 
-  it('can get a list of tweets', () => {
+  it('gets a list of tweets', () => {
     return request(app)
       .get('/tweets')
       .then(res => res.body)
