58e53d118b701f2adb8132a0c691ec7df98d53ab
Kate Dameron <katerj33@gmail.com>
2019-03-18 20:29:09 -0700

    updates to cors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
-const PORT = proces.env.PORT || 7891;
+const PORT = process.env.PORT || 7891;
 
 app.listen(PORT, () => {
   console.log('running');
